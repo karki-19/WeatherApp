21d52762255b5219d401da095561a32123a6c602
karki-19 <deepakbisht807@gmail.com>
2023-08-21 11:14:03 +0530

    Added New features

--- scripts.js
+++ scripts.js
@@ -1,73 +1,107 @@
-const showForm = document.querySelector('form')
-const card = document.querySelector('.card')
-const details = document.querySelector('.details')
-const img  = document.querySelector('.time')
-const icon = document.querySelector('.icon img')
+const showForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const img = document.querySelector(".time");
+const icon = document.querySelector(".icon img");
+const body = document.querySelector('.container');
 
+let check = localStorage.getItem("city");
 
-const updateUI = (data)=>{
-     console.log(data)
-    // const citydata = data.citydata
-    // const weatherdata = data.weatherdata
-    // we will destructure the above code
+if(!check)
+{
+ if ("geolocation" in navigator) {
+  // checking if the browser supports geolocation
+  navigator.geolocation.getCurrentPosition(
+    async (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const url = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${key}&q=${latitude},${longitude}`;
 
-    const {citydata,weatherdata} = data
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.AdministrativeArea.EnglishName);
+      cityUpdate(data.AdministrativeArea.EnglishName)
+        .then((data) => updateUI(data))
+        .catch((err) => console.log(err));
 
-    details.innerHTML = `
+      localStorage.setItem("city", data.AdministrativeArea.EnglishName);
+      // return data;
+    },
+    (error) => {
+      console.error("Geolocation error:", error);
+    }
+  );
+} else {
+  console.error("Geolocation not available");
+}
+}
+
+// geoPosition().then((data) => {
+//   updateUI(data);
+// });
+
+const updateUI = (data) => {
+  // const citydata = data.citydata
+  // const weatherdata = data.weatherdata
+  // we will destructure the above code
+
+  const { citydata, weatherdata } = data;
+
+  details.innerHTML = `
     <div class="text-muted text-uppercase text-center details">
     <h5 class="my-3">${citydata.EnglishName}</h5>
     <div class="my-3">${weatherdata.WeatherText}</div>
     <div class="display-4 my-4">
     <span>${weatherdata.Temperature.Metric.Value}</span>
-    <span>&deg;C</span>`
+    <span>&deg;C</span>`;
 
-    if(card.classList.contains('d-none'))
-      card.classList.remove('d-none');
+  if (card.classList.contains("d-none")) card.classList.remove("d-none");
 
-    // update the image here
-    // if(weatherdata.IsDayTime){
-    //   img.setAttribute('src','img/day.svg')
-    // }
-    // else{
-    //     img.setAttribute('src','img/night.svg')
-    // } 
-    // will use the ternary operator this time
+  // update the image here
+  if(weatherdata.IsDayTime){
+    img.setAttribute('src','img/day.svg')
+    body.style.backgroudColor = 'beige'
+  }
+  else{
+      img.setAttribute('src','img/night.svg')
+      body.style.backgroudColor = 'black'
+  }
+//   will use the ternary operator this time
 
-    let weatherData = weatherdata.IsDayTime ? 'img/day.svg':'img/night.svg'
-    img.setAttribute('src',weatherData)
+//   let weatherData = weatherdata.IsDayTime ? "img/day.svg" : "img/night.svg";
+//   img.setAttribute("src", weatherData);
 
-    const abc = `img/icons/${weatherdata.WeatherIcon}.svg`
-    icon.setAttribute('src',abc)
-}
+  const abc = `img/icons/${weatherdata.WeatherIcon}.svg`;
+  icon.setAttribute("src", abc);
+};
 
-const cityUpdate = async city=>{
-    const citydata = await getData(city)
-    // console.log(city.Key);
-    const weatherdata = await weatherUpdate(citydata.Key)
+const cityUpdate = async (city) => {
+  const citydata = await getData(city);
+  // console.log(city.Key);
+  const weatherdata = await weatherUpdate(citydata.Key);
+  console.log(weatherdata);
 
-    return {
-        citydata:citydata,
-        weatherdata :weatherdata
-    }
+  return {
+    citydata: citydata,
+    weatherdata: weatherdata,
+  };
+};
 
-}
+showForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = showForm.city.value.trim();
+  showForm.reset();
+  cityUpdate(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 
-showForm.addEventListener('submit', e =>{
-    e.preventDefault();
-    const city = showForm.city.value.trim();
-    showForm.reset();
-    cityUpdate(city)
-     .then(data => updateUI(data))
-     .catch(err => console.log(err)) 
-    
-    localStorage.setItem('city',city)
+  localStorage.setItem("city", city);
+});
 
-})
+// let check = localStorage.getItem("city");
 
-let check =localStorage.getItem('city')
-
-if(check){
-    cityUpdate(check)
-    .then(data => updateUI(data))
-    .catch(err=> console.log(err))
+if (check) {
+  cityUpdate(check)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 }
